7c530ae92fa2068f2fbf6ce959a14006f3630940
Valeriia Levchenko <valeriialevchenko17@gmail.com>
2024-05-08 17:22:22 +0300

    add sounds & styles on event 'keydown'

---
+++ Drum Kit/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('keydown', function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+    const key = document.querySelector(`.key[data-key="${e.code}"]`);
+    if (!audio) return;
+    audio.currentTime = 0; //rewind to the start
+    audio.play();
+    key.classList.add('playing');
+});

8f589956a0fc3d845fb482925af23d1fc5fa4ce4
Valeriia Levchenko <valeriialevchenko17@gmail.com>
2024-05-08 17:30:19 +0300

    add remove transition

--- Drum Kit/script.js
+++ Drum Kit/script.js
@@ -6,3 +6,10 @@ window.addEventListener('keydown', function (e) {
     audio.play();
     key.classList.add('playing');
 });
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return; //skip if its not a transform
+    this.classList.remove('playing')
+}
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))

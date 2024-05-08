77c145795e209ef6a958298b3248fca85d7844c2
Valeriia Levchenko <valeriialevchenko17@gmail.com>
2024-05-08 20:36:26 +0300

    update script

--- Drum Kit/script.js
+++ Drum Kit/script.js
@@ -1,15 +1,17 @@
-window.addEventListener('keydown', function (e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
     const key = document.querySelector(`.key[data-key="${e.code}"]`);
     if (!audio) return;
     audio.currentTime = 0; //rewind to the start
     audio.play();
     key.classList.add('playing');
-});
+}
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return; //skip if its not a transform
-    this.classList.remove('playing')
+    this.classList.remove('playing');
 }
-const keys = document.querySelectorAll('.key')
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);

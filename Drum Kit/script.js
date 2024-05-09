3fc29d91b8818b3f6715f0c9f22c1eccb543a5c4
Ilya Tikhomirov <76myxomor76@gmail.com>
2024-05-09 14:02:52 +0300

    Fix the sticky keys:
    1. Adding a handler for the keyup event (key release).

--- Drum Kit/script.js
+++ Drum Kit/script.js
@@ -2,16 +2,21 @@ function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.code}"]`);
     const key = document.querySelector(`.key[data-key="${e.code}"]`);
     if (!audio) return;
-    audio.currentTime = 0; //rewind to the start
+    audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
 }
 
 function removeTransition(e) {
-    if (e.propertyName !== 'transform') return; //skip if its not a transform
+    if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
+}
+
+function removePlayingClass() {
+    keys.forEach(key => key.classList.remove('playing'));
 }
 
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
+window.addEventListener('keyup', removePlayingClass);

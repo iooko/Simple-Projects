02ea0ee4eb4652aea7209b7e0a2f23cc3cdf9a9b
Valeriia Levchenko <valeriialevchenko17@gmail.com>
2024-05-10 21:06:01 +0300

    update script

--- Update CSS Variables/script.js
+++ Update CSS Variables/script.js
@@ -1,6 +1,8 @@
-const inputs = document.querySelectorAll('.variables input');
+const inputs = document.querySelectorAll('.block input');
+
 function handleUpdate() {
-    console.log(this.value)
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value+suffix);
 }
-inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('change',  handleUpdate))
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))

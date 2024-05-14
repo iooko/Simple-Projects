fe2ee8aeb86bbaa9ae26346238519ae89b9e6f71
Valeriia Levchenko <valeriialevchenko17@gmail.com>
2024-05-14 22:40:11 +0300

    update script

--- HTML Canvas/script.js
+++ HTML Canvas/script.js
@@ -1,2 +1,17 @@
 const canvas = document.querySelector('#draw');
 const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context.strokeStyle = '#BADA55';
+context.lineJoin = 'round';
+context.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+    if (!isDrawing) return;
+}
+
+canvas.addEventListener('mousemove', draw)

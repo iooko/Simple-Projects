485597fd66754aed86a78c9ed4fcb189144e3f15
Valeriia Levchenko <valeriialevchenko17@gmail.com>
2024-05-20 21:14:22 +0300

    update script

--- Checkbox/script.js
+++ Checkbox/script.js
@@ -1,17 +1,12 @@
 const checkboxes = document.querySelectorAll('.inbox input[type=checkbox]')
 
-//??? broken code
 let lastChecked;
 function handleCheck(e) {
     let inBetween = false;
     if (e.shiftKey && this.checked) {
         checkboxes.forEach(checkbox => {
-            if (checkbox === this || checkbox === lastChecked) {
-                inBetween = !inBetween;
-            }
-            if (inBetween) {
-                checkbox.checked = true;
-            }
+            if (checkbox === this || checkbox === lastChecked) inBetween = !inBetween;
+            if (inBetween) checkbox.checked = true;
         });
     }
     lastChecked = this;
